4c963f8010dd260a022812262256bc552a77ff29
ocoleman <owencoleman@outlook.ie>
2020-04-18 22:31:21 +0100

    Testing Write function

--- Analysis.md
+++ Analysis.md
@@ -1,8 +1,8 @@
 Analysis of the Dataset.
 
 PART I: Histogram Analysis
-
 Petal Length
+
  <img src='https://github.com/ocoleman/pns-project2020/blob/master/plots/hist-petal_length.png' alt='Histogram-Petal Length'>
 As we can see from the histogram above, the Petal length of the Setosa flowers are tightly situated between 1 and 2 centimetres, with the majority being around the 1.5-centimetre mark.
 The length of the other two species cover a much larger range, between 3 centimetres and just under 7 centimetres. 
--- analysis.py
+++ analysis.py
@@ -12,7 +12,8 @@ def summary():
         f.write("\n")
         f.write("\nPART I: Histogram Analysis")
         f.write("\n")
-        f.write("\nPetal Length")
+        f.write("Petal Length")
+        f.write("\n")
         f.write("\n <img src='https://github.com/ocoleman/pns-project2020/blob/master/plots/hist-petal_length.png' alt='Histogram-Petal Length'>")
         f.write("""\nAs we can see from the histogram above, the Petal length of the Setosa flowers are tightly situated between 1 and 2 centimetres, with the majority being around the 1.5-centimetre mark.
 The length of the other two species cover a much larger range, between 3 centimetres and just under 7 centimetres. 
