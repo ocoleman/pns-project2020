6fb115bafe42e8a664bd5fa04683fc4d26893ec5
ocoleman <owencoleman@outlook.ie>
2020-04-18 22:37:19 +0100

    summary() test

--- Analysis.md
+++ Analysis.md
@@ -1,10 +1,10 @@
-#Analysis of the Dataset.
+<h1>Analysis of the Dataset.</h1>
 
-##PART I: Histogram Analysis
+<h2>PART I: Histogram Analysis</h2>
 
-###Petal Length
+<h3>Petal Length</h3>
 
- <img src='https://github.com/ocoleman/pns-project2020/blob/master/plots/hist-petal_length.png' alt='Histogram-Petal Length'>
+ <img src='https://github.com/ocoleman/pns-project2020/blob/master/plots/hist-petal_length.png' alt='Histogram-Petal Length' align='middle'>
 As we can see from the histogram above, the Petal length of the Setosa flowers are tightly situated between 1 and 2 centimetres, with the majority being around the 1.5-centimetre mark.
 The length of the other two species cover a much larger range, between 3 centimetres and just under 7 centimetres. 
 Despite this it seems the petal length of the Iris Virginica is easily distinguishable as the largest of the 3 species of flower.
--- analysis.py
+++ analysis.py
@@ -8,14 +8,14 @@ import matplotlib.pyplot as plt
 
 def summary():
     with open('Analysis.md', 'w') as f:
-        f.write("#Analysis of the Dataset.")
+        f.write("<h1>Analysis of the Dataset.</h1>")
         f.write("\n")
-        f.write("\n##PART I: Histogram Analysis")
+        f.write("\n<h2>PART I: Histogram Analysis</h2>")
         f.write("\n")
         f.write("\n")
-        f.write("###Petal Length")
+        f.write("<h3>Petal Length</h3>")
         f.write("\n")
-        f.write("\n <img src='https://github.com/ocoleman/pns-project2020/blob/master/plots/hist-petal_length.png' alt='Histogram-Petal Length'>")
+        f.write("\n <img src='https://github.com/ocoleman/pns-project2020/blob/master/plots/hist-petal_length.png' alt='Histogram-Petal Length' align='middle'>")
         f.write("""\nAs we can see from the histogram above, the Petal length of the Setosa flowers are tightly situated between 1 and 2 centimetres, with the majority being around the 1.5-centimetre mark.
 The length of the other two species cover a much larger range, between 3 centimetres and just under 7 centimetres. 
 Despite this it seems the petal length of the Iris Virginica is easily distinguishable as the largest of the 3 species of flower.""")
